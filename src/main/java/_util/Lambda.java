7e0f6d02c8b200e279fb5591abf2733091509c1e
kholodkov <aleksey.kholodkov@gmail.com>
2016-03-27 18:46:04 +0300

    Stream + multiply(List, List) + filterList(List)

--- src/main/java/_util/Lambda.java
+++ src/main/java/_util/Lambda.java
@@ -5,9 +5,14 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class Lambda {
+    public  <T> Stream<List<T>> multiply(List<T> fstList, List<T> sndList) {
+        return fstList.stream()
+                .flatMap(fst -> sndList.stream()
+                        .map(snd -> Arrays.asList(fst, snd))
+                );
+    }
 
-    public  <T> Stream<List<T>> multiply(List<T> arg1, List<T> arg2) {
-        return arg1.stream()
-                .flatMap(fst -> arg2.stream().map(snd -> Arrays.asList(fst, snd)));
+    public Stream<String> filterList(List<String> list, int minLength) {
+        return list.stream().filter(string -> string.length() > minLength);
     }
 }
--- src/test/java/_util/LambdaTest.java
+++ src/test/java/_util/LambdaTest.java
@@ -10,11 +10,19 @@ import static org.testng.Assert.*;
 public class LambdaTest {
     private static List<String> literals = Arrays.asList("A", "B", "C");
     private static List<String> numbers = Arrays.asList("1", "2", "3");
+    private static List<String> strings = Arrays.asList("zeroElem", "hello", "li");
 
     @Test
     public void testMultiply() throws Exception {
+        Lambda lambda = new Lambda();
+        System.out.println("=============== multiply START =================");
+        lambda.multiply(literals, numbers).forEach(System.out::println);
+        System.out.println("=============== multiply END ===================");
 
-        new Lambda().multiply(literals, numbers).forEach(System.out::println);
+        System.out.println("");
 
+        System.out.println("=============== filter START ===================");
+        lambda.filterList(strings, 3).forEach(System.out::println);
+        System.out.println("=============== filter END =====================");
     }
 }
