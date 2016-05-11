e61db70c65783b5059fde3dbf186169978cbb1eb
AlekseyKholodkov <aleksey.kholodkov@gmail.com>
2016-05-11 08:12:40 +0300

    improving

--- src/main/java/_concurrency/MultiThreading.java
+++ src/main/java/_concurrency/MultiThreading.java
@@ -1,4 +1,26 @@
 package _concurrency;
 
-public class MultiThreading {
+public class MultiThreading extends Thread {
+    private int x = 2;
+
+    public static void main(String[] args) throws Exception {
+        new MultiThreading().makeItSo();
+    }
+
+    public MultiThreading() {
+        x = 5;
+        start();
+    }
+
+    public void makeItSo() throws Exception {
+        join();
+        x = x - 1;
+        System.out.println(x);
+    }
+
+    public void run() { x *= 2; }
+
+    public void start() {
+        super.start();
+    }
 }
