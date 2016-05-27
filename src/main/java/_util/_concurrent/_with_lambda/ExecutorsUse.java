47042ec4dc10b4f2d17f2e2eb040d05eba5a74b0
AlekseyKholodkov <aleksey.kholodkov@gmail.com>
2016-05-27 22:47:40 +0300

    concurrent ExecutorService

--- src/main/java/_util/_concurrent/_with_lambda/ExecutorsUse.java
+++ src/main/java/_util/_concurrent/_with_lambda/ExecutorsUse.java
@@ -1,4 +1,18 @@
 package _util._concurrent._with_lambda;
 
+import java.util.concurrent.*;
+
 public class ExecutorsUse {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        Callable<Integer> callableInt = () -> {
+            TimeUnit.SECONDS.sleep(1);
+            return 123;
+        };
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        Future<Integer> futureInt = executor.submit(callableInt);
+        System.out.println("future.isDone() == " + futureInt.isDone());
+        Integer result = futureInt.get();
+        System.out.println("future.get() == " + result);
+        System.out.println("future.isDone() == " + futureInt.isDone());
+    }
 }
