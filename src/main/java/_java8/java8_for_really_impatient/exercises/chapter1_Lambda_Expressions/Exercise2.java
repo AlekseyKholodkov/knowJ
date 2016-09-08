ab898b33af251c88b537fda5692e7bc526179736
Aleksey Kholodkov <aleksey.kholodkov@gmail.com>
2016-09-08 13:23:10 +0300

    IO Java7 vs Java8. Refactoring

--- src/main/java/_java8/java8_for_really_impatient/exercises/chapter1_Lambda_Expressions/Exercise2.java
+++ src/main/java/_java8/java8_for_really_impatient/exercises/chapter1_Lambda_Expressions/Exercise2.java
@@ -1,34 +1,13 @@
 package _java8.java8_for_really_impatient.exercises.chapter1_Lambda_Expressions;
 
-import org.openjdk.jmh.annotations.Benchmark;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Exercise2 {
-    public File[] getAllSubdirectories(String path) {
-        return new File(path).listFiles(File::isDirectory);
-    }
 
-    public List<File> getAllSubdirectoriesIn8Style(String path) {
-        File file = new File(path);
-        List<File> listFiles = Arrays.asList(file.listFiles());
-        List<File> subdirectories = listFiles.stream()
-                .filter(File::isDirectory)
-                .collect(Collectors.toList());
-        return subdirectories;
-    }
-
-    @Benchmark
-    public void measureName() {
-        System.out.println("Benchmarks");
-    }
-
-    public File[] getAllSubdirectoriesIn7Style(String path) {
-//        String path = "d:\\";
+    public File[] getAllSubdirectoriesInJava7Style(String path) {
         File file = new File(path);
         File[] listFiles = file.listFiles();
         List<File> subdirectories = new ArrayList<>();
@@ -37,6 +16,17 @@ public class Exercise2 {
                 subdirectories.add(fl);
             }
         }
-        return subdirectories.toArray(new File[10]);
+        return subdirectories.toArray(new File[subdirectories.size()]);
+    }
+
+    public File[] getAllSubdirectoriesInJava8Style(String path) {
+        File file = new File(path);
+        List<File> listFiles = Arrays.asList(file.listFiles());
+        return Arrays.stream(file.listFiles())
+                .filter(File::isDirectory).toArray(File[]::new);
+    }
+
+    public File[] getAllSubdirectoriesInJava8StyleMethodReference(String path) {
+        return new File(path).listFiles(File::isDirectory);
     }
 }
--- src/test/java/_java8/java8_for_really_impatient/exercises/chapter1_Lambda_Expressions/Exercise2Test.java
+++ src/test/java/_java8/java8_for_really_impatient/exercises/chapter1_Lambda_Expressions/Exercise2Test.java
@@ -3,19 +3,39 @@ package _java8.java8_for_really_impatient.exercises.chapter1_Lambda_Expressions;
 import org.testng.annotations.Test;
 
 import java.io.File;
-import java.util.List;
-
-import static org.testng.Assert.*;
 
 public class Exercise2Test {
     @Test
-    public void testGetAllSubdirectories() throws Exception {
+    public void testGetAllSubdirectoriesInJava7Style() throws Exception {
         Long startTime = System.nanoTime();
         Long time = 0L;
-        File[] directories = new Exercise2().getAllSubdirectoriesIn7Style("d:\\");
-//        File[] directories = new Exercise2().getAllSubdirectories("d:\\");
+        File[] directories = new Exercise2().getAllSubdirectoriesInJava7Style("d:\\");
         time = System.nanoTime() - startTime;
+        System.out.print("Java 7: ");
         System.out.printf("%,d", time);
+        System.out.println();
+
     }
 
+    @Test
+    public void testGetAllSubdirectoriesInJava8Style() throws Exception {
+        Long startTime = System.nanoTime();
+        Long time = 0L;
+        File[] directories = new Exercise2().getAllSubdirectoriesInJava8Style("d:\\");
+        time = System.nanoTime() - startTime;
+        System.out.print("Java 8: ");
+        System.out.printf("%,d", time);
+        System.out.println();
+    }
+
+    @Test
+    public void testGetAllSubdirectoriesInJava8StyleMethodReference() throws Exception {
+        Long startTime = System.nanoTime();
+        Long time = 0L;
+        File[] directories = new Exercise2().getAllSubdirectoriesInJava8StyleMethodReference("d:\\");
+        time = System.nanoTime() - startTime;
+        System.out.print("Java 8 Method Reference: ");
+        System.out.printf("%,d", time);
+        System.out.println();
+    }
 }
